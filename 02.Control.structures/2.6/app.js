88a3a01d51cf77a13fc09eabbe3b8649eb9f6722
ludovic lacroix <ludovic.lacroix82@gmail.com>
2024-03-31 11:15:58 +0200

    adaptation code js

--- 02.Control.structures/2.6/app.js
+++ 02.Control.structures/2.6/app.js
@@ -4,7 +4,7 @@ var day = ["Dimanche","Lundi","Mardi","Mercredi","Jeudi","Vendredi","Samedi"];
 
     do {
         var number = parseInt(window.prompt("enter a number between 1 and 7"));
-    } while (number >= 8 || isNaN(number));
+    } while (number <=0 || number >= 8 || isNaN(number));
 
         number = number -1;
         alert(day[number]);    
